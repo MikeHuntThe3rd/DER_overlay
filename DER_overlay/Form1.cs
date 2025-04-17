02f5fe6a2311600cb7a4a02b2ab5903fd0d7d49d
MikeHuntThe3rd <103815901+MikeHuntThe3rd@users.noreply.github.com>
2025-04-17 11:25:14 +0200

    Add files via upload

---
+++ DER_overlay/Form1.cs
@@ -0,0 +1,56 @@
+﻿using Gma.System.MouseKeyHook;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DER_overlay
+{
+
+    public partial class Form1 : Form
+    {
+        private IKeyboardMouseEvents globalHook;
+        private OverlayForm overlay;
+        OverlayForm over = new OverlayForm();
+        private IKeyboardMouseEvents gma_hook;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            gma_hook = Hook.GlobalEvents();
+            gma_hook.KeyDown += Gma_hook_KeyDown;
+            over.ShowDialog();
+        }
+
+        private void Gma_hook_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.F14)
+            {
+                over.rev();
+            }
+        }
+
+        private void GlobalHook_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.F)
+            {
+                over.rev();
+            }
+        }
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            gma_hook.KeyDown -= Gma_hook_KeyDown;
+            gma_hook.Dispose();
+        }
+    }
+}
---
+++ DER_overlay/OverlayForm.cs
@@ -0,0 +1,59 @@
+﻿using Gma.System.MouseKeyHook;
+using System;
+using System.Data.SqlTypes;
+using System.Drawing;
+using System.Security.Cryptography.X509Certificates;
+using System.Windows.Forms;
+
+public class OverlayForm : Form
+{
+    public bool show = true;
+    public OverlayForm()
+    {
+        this.FormBorderStyle = FormBorderStyle.None; // No border
+        this.TopMost = true; // Stay on top
+        this.BackColor = Color.LimeGreen; // Choose a color to be transparent
+        this.TransparencyKey = Color.LimeGreen; // Make that color transparent
+        this.WindowState = FormWindowState.Maximized; // Full screen
+        this.ShowInTaskbar = true; // Hide from taskbar
+        this.KeyPreview = true;
+        this.KeyPreview = true;
+        
+        int initialStyle = NativeMethods.GetWindowLong(this.Handle, NativeMethods.GWL_EXSTYLE);
+        NativeMethods.SetWindowLong(this.Handle, NativeMethods.GWL_EXSTYLE,
+            initialStyle | NativeMethods.WS_EX_LAYERED); // NO WS_EX_TRANSPARENT
+    }
+    public void rev()
+    {
+        show = !show;
+        this.Invalidate();
+    }
+    protected override void OnPaint(PaintEventArgs e)
+    {
+        base.OnPaint(e);
+        if (show)
+        {
+            using (Pen pen = new Pen(Color.Red, 20))
+            {
+                float x = e.Graphics.DpiX / 96f;
+                float y = e.Graphics.DpiY / 96f;
+                e.Graphics.DrawEllipse(pen, (ClientSize.Width - 40 * 5 * x) * x, 20 * y + 200, 20, 20);
+            }
+        }
+        
+    }
+    private static class NativeMethods
+    {
+        public const int GWL_EXSTYLE = -20;
+        public const int WS_EX_TRANSPARENT = 0x20;
+        public const int WS_EX_LAYERED = 0x80000;
+
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        public static extern int GetWindowLong(IntPtr hWnd, int nIndex);
+
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        public static extern int SetWindowLong(IntPtr hWnd, int nIndex, int dwNewLong);
+    }
+
+
+}
